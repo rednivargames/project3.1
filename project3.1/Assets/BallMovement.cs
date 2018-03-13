52aa9daa4d468a4f698c7f39dda67c61504e57dd
rajputsanjeev <33745086+rajputsanjeev@users.noreply.github.com>
2018-03-13 14:54:34 +0530

    13-3-18[2:51]

---
+++ project3.1/Assets/BallMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+	public Rigidbody R2;
+	public WheelCollider BallCollider;
+	private int speed=5;
+	public bool Grounded;
+	public Transform CenterPoint;
+	private float Radius = 1f;
+	public LayerMask Ground;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	Grounded = Physics.OverlapSphere(CenterPoint.position,Radius,Ground);
+	
+		R2.velocity = new Vector3 (0f,0f,-speed);
+
+	}
+}
---
+++ project3.1/Assets/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	public Transform Player;
+	public Rigidbody R2;
+	private float Speed=5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	transform.position = new Vector3 (transform.position.x,transform.position.y,Player.position.z);
+		R2.velocity = new Vector3 (0f,0f,-Speed);
+	}
+}
